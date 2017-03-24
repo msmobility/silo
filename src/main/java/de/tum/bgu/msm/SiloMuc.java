4906c6c30e98a8fab35963ed574e09fdb59b5444
Rolf Moeckel <rolf.moeckel@udo.edu>
2017-03-24 17:02:39 +0100

    Simplified class, made it almost identical to SiloMstm.

--- src/main/java/de/tum/bgu/msm/SiloMuc.java
+++ src/main/java/de/tum/bgu/msm/SiloMuc.java
@@ -3,7 +3,6 @@ package de.tum.bgu.msm;
 import com.pb.common.util.ResourceUtil;
 import de.tum.bgu.msm.SyntheticPopulationGenerator.SyntheticPopDe;
 import de.tum.bgu.msm.data.summarizeData;
-import de.tum.bgu.msm.transportModel.MstmTransportModel;
 import org.apache.log4j.Logger;
 
 import java.io.BufferedWriter;
@@ -21,9 +20,6 @@ import java.util.ResourceBundle;
 public class SiloMuc {
 
     // main class
-    public static final String PROPERTIES_RUN_SILO                 = "run.silo.model";
-    public static final String PROPERTIES_RUN_SYNTHETIC_POPULATION = "run.synth.pop.generator";
-    protected static final String PROPERTIES_RUN_TRAVEL_DEMAND_MODEL  = "run.travel.demand.model";
     static Logger logger = Logger.getLogger(SiloMuc.class);
 
 
@@ -33,42 +29,34 @@ public class SiloMuc {
         SiloUtil.setBaseYear(2000);
         ResourceBundle rb = SiloUtil.siloInitialization(args[0]);
         long startTime = System.currentTimeMillis();
-        if (ResourceUtil.getBooleanProperty(rb, PROPERTIES_RUN_SYNTHETIC_POPULATION, false)) {
+        try {
+            logger.info("Starting SILO land use model for MUC");
+            logger.info("Scenario: " + SiloUtil.scenarioName + ", Simulation start year: " + SiloUtil.getStartYear());
             SyntheticPopDe sp = new SyntheticPopDe(rb);
             sp.runSP();
-        }
-        if (ResourceUtil.getBooleanProperty(rb, PROPERTIES_RUN_SILO, false)) {
-            try {
-                logger.info("Starting SILO land use model for MUC");
-                logger.info("Scenario: " + SiloUtil.scenarioName + ", Simulation start year: " + SiloUtil.getStartYear());
-                SiloModel model = new SiloModel(rb);
-                model.runModel();
-                logger.info("Finished SILO.");
-            } catch (Exception e) {
-                logger.error("Error running SILO.");
-                throw new RuntimeException(e);
-            } finally {
-                SiloUtil.trackingFile("close");
-                summarizeData.resultFile("close");
-                summarizeData.resultFileSpatial(rb, "close");
-            }
-        }
-        // if land use model was not selected to run, check if travel demand model shall run only
-        if (ResourceUtil.getBooleanProperty(rb, PROPERTIES_RUN_TRAVEL_DEMAND_MODEL, false)) {
-            MstmTransportModel tdm = new MstmTransportModel(rb);
-            tdm.runTransportModel(-1);  // -1 tells transport model to look up first transport year from properties file
-        }
-        float endTime = SiloUtil.rounder(((System.currentTimeMillis() - startTime) / 60000), 1);
-        int hours = (int) (endTime / 60);
-        int min = (int) (endTime - 60 * hours);
-        logger.info("Runtime: " + hours + " hours and " + min + " minutes.");
-        if (ResourceUtil.getBooleanProperty(rb, SiloModel.PROPERTIES_TRACK_TIME, false)) {
-            String fileName = rb.getString(SiloModel.PROPERTIES_TRACK_TIME_FILE);
-            try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)))) {
-                out.println("Runtime: " + hours + " hours and " + min + " minutes.");
-                out.close();
-            } catch (IOException e) {
-                logger.warn("Could not add run-time statement to time-tracking file.");
+            SiloModel model = new SiloModel(rb);
+            model.runModel();
+            logger.info("Finished SILO.");
+        } catch (Exception e) {
+            logger.error("Error running SILO.");
+            throw new RuntimeException(e);
+        } finally {
+            SiloUtil.trackingFile("close");
+            summarizeData.resultFile("close");
+            summarizeData.resultFileSpatial(rb, "close");
+
+            float endTime = SiloUtil.rounder(((System.currentTimeMillis() - startTime) / 60000), 1);
+            int hours = (int) (endTime / 60);
+            int min = (int) (endTime - 60 * hours);
+            logger.info("Runtime: " + hours + " hours and " + min + " minutes.");
+            if (ResourceUtil.getBooleanProperty(rb, SiloModel.PROPERTIES_TRACK_TIME, false)) {
+                String fileName = rb.getString(SiloModel.PROPERTIES_TRACK_TIME_FILE);
+                try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)))) {
+                    out.println("Runtime: " + hours + " hours and " + min + " minutes.");
+                    out.close();
+                } catch (IOException e) {
+                    logger.warn("Could not add run-time statement to time-tracking file.");
+                }
             }
         }
     }
