dc1ad09f764a3306e51f565ba3923eba89370624
Ana Moreno <atmoreno@users.noreply.github.com>
2017-09-20 08:35:53 +0200

    Adapted one method to read CSV files faster

--- src/main/java/de/tum/bgu/msm/SiloUtil.java
+++ src/main/java/de/tum/bgu/msm/SiloUtil.java
@@ -8,6 +8,7 @@ import com.pb.common.util.ResourceUtil;
 import de.tum.bgu.msm.container.SiloModelContainer;
 import de.tum.bgu.msm.data.summarizeData;
 import de.tum.bgu.msm.events.IssueCounter;
+import de.tum.bgu.msm.utils.TableDataFileReader2;
 import omx.OmxMatrix;
 import omx.hdf5.OmxHdf5Datatype;
 import org.apache.log4j.Logger;
@@ -187,6 +188,32 @@ public class SiloUtil {
         }
         return dataTable;
     }
+
+    public static TableDataSet readCSVfile2 (String fileName) {
+        // read csv file and return as TableDataSet
+        File dataFile = new File(fileName);
+        TableDataSet dataTable;
+        boolean exists = dataFile.exists();
+        if (!exists) {
+            final String msg = "File not found: " + fileName;
+            logger.error(msg);
+//            System.exit(1);
+            throw new RuntimeException(msg) ;
+            // from the perspective of the junit testing infrastructure, a "System.exit(...)" is not a test failure ... and thus not detected.  kai, aug'16
+        }
+        try {
+//            TableDataFileReader reader = TableDataFileReader.createReader(dataFile);
+//            dataTable = reader.readFile(dataFile);
+            TableDataFileReader2 reader = TableDataFileReader2.createReader2(dataFile);
+            dataTable = reader.readFile(dataFile);
+            reader.close();
+        } catch (Exception e) {
+            logger.error("Error reading file " + dataFile);
+            throw new RuntimeException(e);
+        }
+        return dataTable;
+    }
+
 
 
     public static boolean checkIfFileExists (String fileName) {
---
+++ src/main/java/de/tum/bgu/msm/utils/TableDataFileReader2.java
@@ -0,0 +1,62 @@
+package de.tum.bgu.msm.utils;
+
+/**
+ * Created by kii on 9/15/2017.
+ */
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+/*
+import com.pb.common.datafile.*;
+import com.pb.common.datafile.FileType;
+import com.pb.common.datafile.TableDataSet;
+*/
+
+import com.pb.common.datafile.*;
+import de.tum.bgu.msm.utils.CSVFileReader2;
+
+import java.io.File;
+import java.io.IOException;
+
+public abstract class TableDataFileReader2 extends TableDataReader {
+    private File myDirectory;
+
+    public TableDataFileReader2() {
+
+    }
+
+    //    public static com.pb.common.datafile.TableDataFileReader createReader(FileType type) {
+//        com.pb.common.datafile.TableDataFileReader reader = null;
+    public static TableDataFileReader2 createReader2(FileType type) {
+        TableDataFileReader2 reader = null;
+        if(!type.equals(FileType.CSV)) {
+            throw new RuntimeException("Invalid file type: " + type);
+        }
+        reader = new CSVFileReader2();
+
+        return (TableDataFileReader2) reader;
+    }
+
+    public static TableDataFileReader2 createReader2(File file) {
+        TableDataFileReader2 reader = null;
+        String fileName = file.getName();
+        if(!fileName.endsWith(".csv")) {
+            throw new RuntimeException("Could not determine file type for: " + fileName);
+        }
+        reader = new CSVFileReader2();
+
+        return (TableDataFileReader2)reader;
+    }
+
+    public abstract TableDataSet readFile(File var1) throws IOException;
+
+    public void setMyDirectory(File myDirectory) {
+        this.myDirectory = myDirectory;
+    }
+
+    public File getMyDirectory() {
+        return this.myDirectory;
+    }
+}
