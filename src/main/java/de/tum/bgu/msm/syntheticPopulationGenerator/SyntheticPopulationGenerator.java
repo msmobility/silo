62abb9af4d77615487d6040ed7dae97b4b37ed0c
Ana Moreno <atmoreno@users.noreply.github.com>
2017-11-28 08:44:09 +0100

    fixed bug in synthetic population generator that pointed to Maryland implementation (MUC vs. MUNICH)

--- src/main/java/de/tum/bgu/msm/syntheticPopulationGenerator/SyntheticPopulationGenerator.java
+++ src/main/java/de/tum/bgu/msm/syntheticPopulationGenerator/SyntheticPopulationGenerator.java
@@ -1,11 +1,11 @@
 package de.tum.bgu.msm.syntheticPopulationGenerator;
 
 
-import com.pb.common.util.ResourceUtil;
+import de.tum.bgu.msm.SiloModel;
+import de.tum.bgu.msm.properties.Properties;
 import de.tum.bgu.msm.syntheticPopulationGenerator.capeTown.SyntheticPopCT;
 import de.tum.bgu.msm.syntheticPopulationGenerator.maryland.SyntheticPopUs;
 import de.tum.bgu.msm.syntheticPopulationGenerator.munich.SyntheticPopDe;
-import de.tum.bgu.msm.syntheticPopulationGenerator.munichCity.SyntheticPopMuc;
 import org.apache.log4j.Logger;
 
 
@@ -14,9 +14,7 @@ import java.util.ResourceBundle;
 public class SyntheticPopulationGenerator {
 
     static Logger logger = Logger.getLogger(SyntheticPopulationGenerator.class);
-    //public enum Implementation {MUC, MSTM, CAPE_TOWN, MSP, MUC_CITY};
     private final ResourceBundle rb;
-    protected static final String PROPERTIES_IMPLEMENTATION_SYNTHETIC_POPULATION     = "syn.pop.implementation";
 
     public SyntheticPopulationGenerator( ResourceBundle rb) {
         this.rb = rb;// set up counter for any issues during initial setup
@@ -25,18 +23,23 @@ public class SyntheticPopulationGenerator {
     public void run(){
 
         SyntheticPopI syntheticPop;
-        String implementation = ResourceUtil.getProperty(rb, PROPERTIES_IMPLEMENTATION_SYNTHETIC_POPULATION);
-
-        if (implementation.equals("MUC")){
-            syntheticPop = new SyntheticPopDe(rb);
-        } else if (implementation.equals("CAPE_TOWN")){
-            syntheticPop = new SyntheticPopCT(rb);
-        } else if (implementation.equals("MUC_CITY")){
-            syntheticPop = new SyntheticPopMuc(rb);
-        } else {
-            syntheticPop = new SyntheticPopUs(rb);
+        SiloModel.Implementation imp = Properties.get().main.implementation;
+        switch (imp){
+            case MUNICH:
+                syntheticPop = new SyntheticPopDe(rb);
+                break;
+            case MARYLAND:
+                syntheticPop = new SyntheticPopUs(rb);
+                break;
+            case CAPE_TOWN:
+                syntheticPop = new SyntheticPopCT(rb);
+                break;
+            case MSP:
+                syntheticPop = new SyntheticPopUs(rb);
+                break;
+            default:
+                throw new RuntimeException("Synthetic population implementation not set");
         }
-
         syntheticPop.runSP();
 
     }
