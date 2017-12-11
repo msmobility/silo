021517cf097dc5aed7d9d0b2463060755caf1062
Nico <nkuehnel2@gmail.com>
2017-12-11 12:07:12 +0100

    ignore munich test for now

--- src/test/java/de/tum/bgu/msm/SiloMunichTest.java
+++ src/test/java/de/tum/bgu/msm/SiloMunichTest.java
@@ -1,18 +1,17 @@
 package de.tum.bgu.msm;
 
-import de.tum.bgu.msm.SiloMuc;
 import de.tum.bgu.msm.transportModel.SiloTestUtils;
 import de.tum.bgu.msm.transportModel.matsim.MatsimTestUtils;
 import de.tum.bgu.msm.transportModel.mito.SiloMitoTest;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class SiloMunichTest {
 
     private static final Logger log = Logger.getLogger(SiloMitoTest.class);
-
 
     @Rule
     public MatsimTestUtils utils = new MatsimTestUtils();
@@ -22,9 +21,9 @@ public class SiloMunichTest {
      * This test should test Silo together with Mito in Munich.
      */
 
+    @Ignore
     @Test
     public final void testMainReduced() {
-
 
         SiloTestUtils.cleanUpMicrodataFiles();
         SiloTestUtils.cleanUpOtherFiles();
