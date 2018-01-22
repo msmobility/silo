ada8dd53ef187f888afebac98c962f1298a729fc
Rolf Moeckel <rolf.moeckel@udo.edu>
2018-01-22 16:49:57 +0100

    Death probability class converted to JavaScript.
    #fixes 11

--- src/main/java/de/tum/bgu/msm/demography/DeathModel.java
+++ src/main/java/de/tum/bgu/msm/demography/DeathModel.java
@@ -11,10 +11,13 @@ import de.tum.bgu.msm.properties.Properties;
 import org.apache.log4j.Logger;
 
 import java.io.File;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 /**
- * @author Greg Erhardt 
+ * @author Greg Erhardt, Rolf Moeckel
  * Created on Dec 2, 2009
+ * Revised on Jan 19, 2018
  *
  */
 public class DeathModel {
@@ -22,7 +25,7 @@ public class DeathModel {
     private static Logger traceLogger = Logger.getLogger("trace");
 
     private final HouseholdDataManager householdDataManager;
-	private double[] deathProbability;
+	private double[][] deathProbability;
 
     public DeathModel(HouseholdDataManager householdDataManager) {
         this.householdDataManager = householdDataManager;
@@ -32,50 +35,59 @@ public class DeathModel {
 	private void setupDeathModel() {
 
 		// read properties
-		int deathModelSheetNumber = Properties.get().demographics.deathModelSheet;
+//		int deathModelSheetNumber = Properties.get().demographics.deathModelSheet;
         boolean logCalculation = Properties.get().demographics.logDeathCalculation;
 
 		// initialize UEC
-        UtilityExpressionCalculator deathModel = new UtilityExpressionCalculator(new File(Properties.get().main.baseDirectory + Properties.get().demographics.uecFileName),
-        		deathModelSheetNumber,
-                Properties.get().demographics.dataSheet,
-        		SiloUtil.getRbHashMap(),
-        		DeathDMU.class);
+//        UtilityExpressionCalculator deathModel = new UtilityExpressionCalculator(new File(Properties.get().main.baseDirectory + Properties.get().demographics.uecFileName),
+//        		deathModelSheetNumber,
+//                Properties.get().demographics.dataSheet,
+//        		SiloUtil.getRbHashMap(),
+//        		DeathDMU.class);
         
-		DeathDMU deathDmu = new DeathDMU();
+//		DeathDMU deathDmu = new DeathDMU();
 
-		// everything is available	
-		int numAlts = deathModel.getNumberOfAlternatives();
-		int[] death2Avail = new int[numAlts+1]; 
-        for (int i=1; i < death2Avail.length; i++) {
-            death2Avail[i] = 1;
-        }
-        
-        PersonType[] types = PersonType.values();
-        deathProbability = new double[types.length];
-        for (int i=0; i<types.length; i++) {
+//		// everything is available
+//		int numAlts = deathModel.getNumberOfAlternatives();
+//		int[] death2Avail = new int[numAlts+1];
+//        for (int i=1; i < death2Avail.length; i++) {
+//            death2Avail[i] = 1;
+//        }
 
-        	// set DMU attributes
-        	deathDmu.setType(types[i]);
+        Reader reader = new InputStreamReader(this.getClass().getResourceAsStream("DeathProbabilityCalc"));
+        DeathJSCalculator calculator = new DeathJSCalculator(reader);
+
+        deathProbability = new double[101][2];  // probabilities by 101 age classes and sex
+        for (int age = 0; age < deathProbability.length; age++) {
+            for (int sex = 0; sex < deathProbability[0].length; sex++) {
+                int sexCode = sex + 1;  // (1 = male, 2 = female)
+                deathProbability[age][sex] = calculator.calculateDeathProbability(age, sexCode);
+            }
+//        	// set DMU attributes
+//        	deathDmu.setType(types[i]);
         	
             // There is only one alternative, and the utility is 
     		// really the probability of dying
-    		double util[] = deathModel.solve(deathDmu.getDmuIndexValues(), deathDmu, death2Avail);
-    		deathProbability[i] = util[0];
+//    		double util[] = deathModel.solve(deathDmu.getDmuIndexValues(), deathDmu, death2Avail);
+//    		deathProbability[i] = util[0];
             
             if (logCalculation) {
                 // log UEC values for each person type
-                deathModel.logAnswersArray(traceLogger, "Death Model for Person Type " + types[i].toString());
+                traceLogger.warn("Logging not implemented for DeathModel.java!");
+//                deathModel.logAnswersArray(traceLogger, "Death Model for Person Type " + types[i].toString());
             }
         }
 	}
+
 
 	public void chooseDeath(int perId, SiloDataContainer dataContainer) {
         // simulate if person with ID perId dies in this simulation period
 
         Person per = Person.getPersonFromId(perId);
         if (!EventRules.ruleDeath(per)) return;  // Person has moved away
-        if (SiloUtil.getRandomNumberAsDouble() < deathProbability[per.getType().ordinal()]) {
+        int age = Math.min(per.getAge(), 100);
+        int sexIndex = per.getGender() - 1;
+        if (SiloUtil.getRandomNumberAsDouble() < deathProbability[age][sexIndex]) {
             if (per.getWorkplace() > 0) per.quitJob(true, dataContainer.getJobData());
             Household hhOfPersonToDie = per.getHh();
             int hhId = hhOfPersonToDie.getId();
---
+++ src/test/java/de/tum/bgu/msm/models/javascript/DeathTest.java
@@ -0,0 +1,35 @@
+package de.tum.bgu.msm.models.javascript;
+
+import de.tum.bgu.msm.demography.DeathJSCalculator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class DeathTest {
+    private DeathJSCalculator calculator;
+
+    @Before
+    public void setup() {
+        Reader reader = new InputStreamReader(this.getClass().getResourceAsStream("DeathProbabilityCalc"));
+        calculator = new DeathJSCalculator (reader);
+    }
+
+    @Test
+    public void testModelOne() {
+        Assert.assertEquals(0.00068366, calculator.calculateDeathProbability(31, 1), 0.);
+    }
+
+    @Test
+    public void testModelFailures() {
+        Assert.assertEquals(0.410106, calculator.calculateDeathProbability(200, 0), 0.);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testModelFailuresTwo() {
+        calculator.calculateDeathProbability(-2, 1);
+    }
+
+}
