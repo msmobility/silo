e4b18d91ca82c67431a4e52c62fb44d5cfce02d9
Nico <nkuehnel2@gmail.com>
2018-02-26 13:00:05 +0100

    put demolition calc test back

--- src/test/java/de/tum/bgu/msm/models/javascript/DemolitionTest.java
+++ src/test/java/de/tum/bgu/msm/models/javascript/DemolitionTest.java
@@ -1,5 +1,7 @@
 package de.tum.bgu.msm.models.javascript;
 
+import de.tum.bgu.msm.data.Dwelling;
+import de.tum.bgu.msm.data.DwellingType;
 import de.tum.bgu.msm.realEstate.DemolitionJSCalculator;
 import org.junit.Assert;
 import org.junit.Before;
@@ -12,20 +14,26 @@ import java.io.Reader;
 public class DemolitionTest {
 
     private DemolitionJSCalculator calculator;
+    private Dwelling dwelling1;
+    private Dwelling dwelling2;
+
 
     @Before
     public void setup() {
         Reader reader = new InputStreamReader(this.getClass().getResourceAsStream("DemolitionCalc"));
         calculator = new DemolitionJSCalculator(reader);
+        dwelling1 = new Dwelling(1,1,1, DwellingType.SFD, 1,1,1,1,1);
+        dwelling2 = new Dwelling(1,1,1, DwellingType.SFD, 1,5,1,1,5);
+
     }
 
-//    @Test
-//    public void testModel() {
-//        Assert.assertEquals(0.0001, calculator.calculateDemolitionProbability(true, 1), 0.);
-//    }
-//
-//    @Test (expected = RuntimeException.class)
-//    public void testModelFailure() {
-//        calculator.calculateDemolitionProbability(true, 5);
-//    }
+    @Test
+    public void testModel() {
+        Assert.assertEquals(0.0001, calculator.calculateDemolitionProbability(dwelling1,0), 0.);
+    }
+
+    @Test (expected = RuntimeException.class)
+    public void testModelFailure() {
+        calculator.calculateDemolitionProbability(dwelling2, 0);
+    }
 }
