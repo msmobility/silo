9f0e7e398a7cc8d5d04560bd1270c02ac221b30e
cllorca1 <carlos.llorca@tum.de>
2018-07-24 13:59:27 +0200

    javadoc for person roles

--- src/main/java/de/tum/bgu/msm/data/PersonRole.java
+++ src/main/java/de/tum/bgu/msm/data/PersonRole.java
@@ -7,9 +7,18 @@ package de.tum.bgu.msm.data;
  **/
 
 public enum PersonRole {
-    SINGLE,          // does not live with other person aged +/- 15 years: can get married, unlikely to have a child, can leave household if household size > 1
-    MARRIED,         // lives with other person aged +/- 15 years: can get divorced, more likely to have a child
-    CHILD;            // lives with at least one person aged +15 to +40: can leave parental household, unlikely to have a child, can get married
+    /**
+     * does not live with other person aged +/- 15 years: can get married, unlikely to have a child, can leave household if household size > 1
+     */
+    SINGLE,
+    /**
+     * lives with other person aged +/- 15 years: can get divorced, more likely to have a child
+     */
+    MARRIED,
+    /**
+    * lives with at least one person aged +15 to +40: can leave parental household, unlikely to have a child, can get married
+    */
+    CHILD;
 
     @Override
     public String toString() {
