3a5ae2de24a3845ea06af3bd7d87bb4970369815
nkuehnel <nico.kuehnel@tum.de>
2019-01-30 13:28:54 -0500

    updated synthetic population generator for maryland. works with csv PUMS data

---
+++ synthetic-population/src/main/java/de/tum/bgu/msm/syntheticPopulationGenerator/maryland/CreateFreeFlowCarSkim.java
@@ -0,0 +1,102 @@
+package de.tum.bgu.msm.syntheticPopulationGenerator.maryland;
+
+import cern.colt.matrix.tdouble.DoubleMatrix2D;
+import com.vividsolutions.jts.geom.Coordinate;
+import de.tum.bgu.msm.Implementation;
+import de.tum.bgu.msm.data.Zone;
+import de.tum.bgu.msm.data.maryland.GeoDataMstm;
+import de.tum.bgu.msm.data.travelTimes.SkimTravelTimes;
+import de.tum.bgu.msm.io.OmxTravelTimesWriter;
+import de.tum.bgu.msm.io.output.OmxMatrixWriter;
+import de.tum.bgu.msm.models.transportModel.matsim.MatsimTravelTimes;
+import de.tum.bgu.msm.util.matrices.Matrices;
+import de.tum.bgu.msm.utils.SiloUtil;
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.network.io.MatsimNetworkReader;
+import org.matsim.core.router.util.TravelDisutility;
+import org.matsim.core.trafficmonitoring.FreeSpeedTravelTime;
+import org.matsim.utils.leastcostpathtree.LeastCostPathTree;
+import org.matsim.vehicles.Vehicle;
+
+import java.util.*;
+
+/**
+ * Created by zhenpeng on 1/9/2019.
+ */
+public class CreateFreeFlowCarSkim {
+
+    private final static double TIME = 0;
+
+    public static void main(String[] args) {
+
+        SiloUtil.siloInitialization(Implementation.MARYLAND, "T:/2Kuehnel_Nico/synpop/siloMstm.properties");
+        GeoDataMstm geoData = new GeoDataMstm();
+        geoData.readData();
+
+        Network network = NetworkUtils.createNetwork();
+        new MatsimNetworkReader(network).readFile("T:\\2Kuehnel_Nico/networkCleaned_epsg26918.xml.gz");
+
+        Collection<Zone> zones = geoData.getZones().values();
+        System.out.println("after initialization");
+        LeastCostPathTree leastCostPathTree = new LeastCostPathTree(new FreeSpeedTravelTime(), new TravelDisutility() {
+            @Override
+            public double getLinkTravelDisutility(Link link, double v, Person person, Vehicle vehicle) {
+                return 0;
+            }
+
+            @Override
+            public double getLinkMinimumTravelDisutility(Link link) {
+                return 0;
+            }
+        });
+
+
+        final Map<Zone, Node> zoneCalculationNodesMap = new HashMap<>();
+        for (Zone zone : zones) {
+            Coordinate coordinate = zone.getRandomCoordinate();
+            Coord originCoord = new Coord(coordinate.x, coordinate.y);
+            Node originNode = NetworkUtils.getNearestLink(network, originCoord).getToNode();
+            zoneCalculationNodesMap.put(zone, originNode);
+
+        }
+
+        final DoubleMatrix2D matrix = Matrices.doubleMatrix2D(zones, zones);
+
+        for (Zone zone : zones) {
+            System.out.println("Finished zone " + zone.getId());
+            Node originNode = zoneCalculationNodesMap.get(zone);
+            leastCostPathTree.calculate(network, originNode, TIME);
+            Map<Id<Node>, LeastCostPathTree.NodeData> tree = leastCostPathTree.getTree();
+            for (Zone zoneTo : zones) {
+
+                Node toNode;
+                double time;
+                if(zone.equals(zoneTo)) {
+                    double timeSum = 0;
+                    for(int i = 0; i <3; i++) {
+                        Coordinate coordinate = zone.getRandomCoordinate();
+                        Coord originCoord = new Coord(coordinate.x, coordinate.y);
+                        toNode = NetworkUtils.getNearestLink(network, originCoord).getToNode();
+                        timeSum += tree.get(toNode.getId()).getTime();
+                    }
+                    time = timeSum / 3;
+                } else {
+                    toNode = zoneCalculationNodesMap.get(zoneTo);
+                     time = tree.get(toNode.getId()).getTime();
+                }
+
+                matrix.setQuick(zone.getZoneId(), zoneTo.getZoneId(), time);
+
+            }
+        }
+
+        OmxMatrixWriter.createOmxFile("T:/2Kuehnel_Nico/carTravelSkim.omx", zones.stream().mapToInt(de.tum.bgu.msm.data.Id::getId).max().getAsInt()+1);
+        OmxMatrixWriter.createOmxSkimMatrix(matrix, "T:/2Kuehnel_Nico/carTravelSkim.omx", "carSkim");
+    }
+}
