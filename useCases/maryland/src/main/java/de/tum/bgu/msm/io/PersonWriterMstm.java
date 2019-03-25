9a0fcf07d1bb4997579197ce2c7956b11c4ce5bc
Ana Moreno <atmoreno@users.noreply.github.com>
2019-03-25 09:26:14 +0100

    added person writer MSTM to git

---
+++ useCases/maryland/src/main/java/de/tum/bgu/msm/io/PersonWriterMstm.java
@@ -0,0 +1,63 @@
+package de.tum.bgu.msm.io;
+
+import de.tum.bgu.msm.data.household.HouseholdDataManager;
+import de.tum.bgu.msm.data.person.MarylandPerson;
+import de.tum.bgu.msm.data.person.Person;
+import de.tum.bgu.msm.io.output.PersonWriter;
+import de.tum.bgu.msm.utils.SiloUtil;
+import org.apache.log4j.Logger;
+
+import java.io.PrintWriter;
+
+public class PersonWriterMstm implements PersonWriter {
+
+    private final static Logger logger = Logger.getLogger(PersonWriterMstm.class);
+
+    private final HouseholdDataManager householdData;
+
+    public PersonWriterMstm(HouseholdDataManager householdData) {
+        this.householdData = householdData;
+    }
+
+    @Override
+    public void writePersons(String path) {
+
+        logger.info("  Writing person file to " + path);
+        PrintWriter pwp = SiloUtil.openFileForSequentialWriting(path, false);
+        pwp.print("id,hhid,age,gender,relationShip,occupation,driversLicense,workplace,income");
+        pwp.print(",");
+        pwp.print("race");
+
+        pwp.println();
+        for (Person pp : householdData.getPersons()) {
+            pwp.print(pp.getId());
+            pwp.print(",");
+            pwp.print(pp.getHousehold().getId());
+            pwp.print(",");
+            pwp.print(pp.getAge());
+            pwp.print(",");
+            pwp.print(pp.getGender().getCode());
+            pwp.print(",\"");
+            String role = pp.getRole().toString();
+            pwp.print(role);
+            pwp.print("\",");
+            pwp.print(pp.getOccupation().getCode());
+            pwp.print(",");
+            pwp.print(pp.hasDriverLicense());
+            pwp.print(",");
+            pwp.print(pp.getJobId());
+            pwp.print(",");
+            pwp.print(pp.getIncome());
+            pwp.print(",");
+            pwp.print(((MarylandPerson)pp).getRace().toString());
+            pwp.println();
+            if (pp.getId() == SiloUtil.trackPp) {
+                SiloUtil.trackingFile("Writing pp " + pp.getId() + " to micro data file.");
+                SiloUtil.trackWriter.println(pp.toString());
+            }
+        }
+        pwp.close();
+
+
+    }
+}
