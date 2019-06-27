3918ce1fea313b882821191a7bb3da6f6c5bc233
Rolf Moeckel <rolf.moeckel@tum.de>
2019-06-27 22:48:15 +0200

    Fixed test for revised construction demand calculation at the regional level.

--- siloCore/src/test/java/de/tum/bgu/msm/models/javascript/ConstructionDemandTest.java
+++ siloCore/src/test/java/de/tum/bgu/msm/models/javascript/ConstructionDemandTest.java
@@ -1,29 +1,26 @@
-//package de.tum.bgu.msm.models.javascript;
-//
-//import de.tum.bgu.msm.data.dwelling.DefaultDwellingTypeImpl;
-//import de.tum.bgu.msm.models.realEstate.construction.DefaultConstructionDemandStrategy;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import javax.script.ScriptException;
-//import java.io.InputStreamReader;
-//import java.io.Reader;
-//
-//public class ConstructionDemandTest {
-//    private DefaultConstructionDemandStrategy calculator;
-//
-//    @Before
-//    public void setup() {
-//        Reader reader = new InputStreamReader(this.getClass().getResourceAsStream("ConstructionDemandCalcMstm"));
-//        calculator = new DefaultConstructionDemandStrategy(reader);
-//    }
-//
-//    @Test
-//    public void testModelOne() throws ScriptException {
-//        Assert.assertEquals(0.00501, calculator.calculateConstructionDemand(0.05, DefaultDwellingTypeImpl.MF234), 0.00001);
-//    }
-//}
-//
-//
-//
+package de.tum.bgu.msm.models.javascript;
+
+import de.tum.bgu.msm.data.dwelling.DefaultDwellingTypeImpl;
+import de.tum.bgu.msm.models.realEstate.construction.DefaultConstructionDemandStrategy;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.script.ScriptException;
+
+public class ConstructionDemandTest {
+    private DefaultConstructionDemandStrategy calculator;
+
+    @Before
+    public void setup() {
+        calculator = new DefaultConstructionDemandStrategy();
+    }
+
+    @Test
+    public void testModelOne() throws ScriptException {
+        Assert.assertEquals(0.025, calculator.calculateConstructionDemand(0.015, DefaultDwellingTypeImpl.SFD), 0.00001);
+    }
+    public void testModelTwo() throws ScriptException {
+        Assert.assertEquals(0.003723333, calculator.calculateConstructionDemand(0.05, DefaultDwellingTypeImpl.SFD), 0.00001);
+    }
+}
