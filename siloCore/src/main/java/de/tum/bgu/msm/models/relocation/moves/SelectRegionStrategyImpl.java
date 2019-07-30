7294b21a42593e2ad25b441b8544512aa0896d6a
moeckel <xz9a3JatXvDj>
2019-07-30 16:31:56 +0200

    Added Utility calculations to core, because Perth Housing Search Implementation has been made the default implementation, which needs these utility calculations. Also simplified calculation of regional utilities.

--- siloCore/src/main/java/de/tum/bgu/msm/models/relocation/moves/SelectRegionStrategyImpl.java
+++ siloCore/src/main/java/de/tum/bgu/msm/models/relocation/moves/SelectRegionStrategyImpl.java
@@ -1,6 +1,7 @@
 package de.tum.bgu.msm.models.relocation.moves;
 
 import de.tum.bgu.msm.data.household.IncomeCategory;
+import de.tum.bgu.msm.models.ScriptInputProvider;
 import de.tum.bgu.msm.util.js.JavaScriptCalculator;
 
 import java.io.InputStreamReader;
@@ -8,8 +9,7 @@ import java.io.Reader;
 
 public class SelectRegionStrategyImpl extends JavaScriptCalculator<Double> implements SelectRegionStrategy {
 
-    private final static Reader reader
-            = new InputStreamReader(DwellingUtilityStrategyImpl.class.getResourceAsStream("SelectRegionCalc"));
+    private final static Reader reader = new InputStreamReader(ScriptInputProvider.getSelectRegionScriptInput());
 
 
     public SelectRegionStrategyImpl() {
