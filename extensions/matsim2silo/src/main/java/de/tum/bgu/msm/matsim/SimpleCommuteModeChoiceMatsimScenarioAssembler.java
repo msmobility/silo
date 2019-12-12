0dfd3e2187d16da344d1c8218896b2ba5394c877
dziemke <ziemke@vsp.tu-berlin.de>
2019-12-12 18:43:52 +0100

    revise scenario assembler, less hardcoded user settings

--- extensions/matsim2silo/src/main/java/de/tum/bgu/msm/matsim/SimpleCommuteModeChoiceMatsimScenarioAssembler.java
+++ extensions/matsim2silo/src/main/java/de/tum/bgu/msm/matsim/SimpleCommuteModeChoiceMatsimScenarioAssembler.java
@@ -1,10 +1,6 @@
 package de.tum.bgu.msm.matsim;
 
 import de.tum.bgu.msm.container.DataContainer;
-import de.tum.bgu.msm.data.Location;
-import de.tum.bgu.msm.data.Region;
-import de.tum.bgu.msm.data.TravelTimesWrapper;
-import de.tum.bgu.msm.data.Zone;
 import de.tum.bgu.msm.data.dwelling.Dwelling;
 import de.tum.bgu.msm.data.dwelling.RealEstateDataManager;
 import de.tum.bgu.msm.data.household.Household;
@@ -13,34 +9,23 @@ import de.tum.bgu.msm.data.job.Job;
 import de.tum.bgu.msm.data.job.JobDataManager;
 import de.tum.bgu.msm.data.person.Occupation;
 import de.tum.bgu.msm.data.person.Person;
-import de.tum.bgu.msm.data.travelTimes.SkimTravelTimes;
 import de.tum.bgu.msm.data.travelTimes.TravelTimes;
 import de.tum.bgu.msm.models.modeChoice.CommuteModeChoice;
 import de.tum.bgu.msm.models.modeChoice.CommuteModeChoiceMapping;
 import de.tum.bgu.msm.properties.Properties;
-import de.tum.bgu.msm.util.matrices.IndexedDoubleMatrix2D;
 import de.tum.bgu.msm.utils.SiloUtil;
-import de.tum.bgu.msm.utils.TravelTimeUtil;
 import org.apache.log4j.Logger;
 import org.locationtech.jts.geom.Coordinate;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.TransportMode;
-import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Plan;
 import org.matsim.api.core.v01.population.Population;
 import org.matsim.api.core.v01.population.PopulationFactory;
 import org.matsim.core.config.Config;
-import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
-import org.matsim.core.config.groups.VspExperimentalConfigGroup;
-import org.matsim.core.population.PopulationUtils;
-import org.matsim.core.router.util.TravelTime;
-import org.matsim.core.scenario.MutableScenario;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.vehicles.Vehicle;
 
 public class SimpleCommuteModeChoiceMatsimScenarioAssembler implements MatsimScenarioAssembler {
 
@@ -48,7 +33,6 @@ public class SimpleCommuteModeChoiceMatsimScenarioAssembler implements MatsimSce
 
     private final DataContainer dataContainer;
     private CommuteModeChoice commuteModeChoice;
-
     private final Properties properties;
 
     public SimpleCommuteModeChoiceMatsimScenarioAssembler(DataContainer dataContainer, Properties properties, CommuteModeChoice commuteModeChoice) {
@@ -57,22 +41,32 @@ public class SimpleCommuteModeChoiceMatsimScenarioAssembler implements MatsimSce
         this.properties = properties;
     }
 
-    private Population generateDemand(TravelTimes travelTimes) {
-        logger.info("Starting creating a MATSim population.");
+    @Override
+    public Scenario assembleScenario(Config matsimConfig, int year, TravelTimes travelTimes) {
+        logger.info("Starting creating MATSim scenario.");
+        double populationScalingFactor = properties.transportModel.matsimScaleFactor;
+        SiloMatsimUtils.checkSiloPropertiesAndMatsimConfigConsistency(matsimConfig, properties);
 
-        HouseholdDataManager householdDataManager = dataContainer.getHouseholdDataManager();
+//        if(dataContainer.getTravelTimes() instanceof SkimTravelTimes) {
+//
+//        }
+//        if(year == properties.main.startYear) {
+//            logger.info("Referring to the simple MATSim scenario assembler for the base year as no travel times are available yet.");
+//            return new SimpleMatsimScenarioAssembler(dataContainer, properties).assembleScenario(initialMatsimConfig, year);
+//        } else {
+//            travelTimes = dataContainer.getTravelTimes();
+//        }
 
-        Population matsimPopulation = PopulationUtils.createPopulation(ConfigUtils.createConfig());
+        Scenario scenario = ScenarioUtils.loadScenario(matsimConfig);
+        Population matsimPopulation = scenario.getPopulation();
         PopulationFactory matsimPopulationFactory = matsimPopulation.getFactory();
 
+        HouseholdDataManager householdDataManager = dataContainer.getHouseholdDataManager();
         JobDataManager jobDataManager = dataContainer.getJobDataManager();
-
         RealEstateDataManager realEstateDataManager = dataContainer.getRealEstateDataManager();
 
         for(Household household: householdDataManager.getHouseholds()) {
-            if (SiloUtil.getRandomNumberAsDouble() > properties.transportModel.matsimScaleFactor) {
-                // e.g. if scalingFactor = 0.01, there will be a 1% chance that the loop is not
-                // continued in the next step, i.e. that the person is added to the population
+            if (SiloUtil.getRandomNumberAsDouble() > populationScalingFactor) {
                 continue;
             }
             Dwelling dwelling = realEstateDataManager.getDwelling(household.getDwellingId());
@@ -84,7 +78,6 @@ public class SimpleCommuteModeChoiceMatsimScenarioAssembler implements MatsimSce
                     continue;
                 }
                 if(commuteModeChoiceMapping.getMode(person).mode.equals(TransportMode.car)) {
-
                     Coordinate dwellingCoordinate;
                     if (dwelling != null && dwelling.getCoordinate() != null) {
                         dwellingCoordinate = dwelling.getCoordinate();
@@ -125,10 +118,9 @@ public class SimpleCommuteModeChoiceMatsimScenarioAssembler implements MatsimSce
                     matsimPlan.addActivity(activity3);
                 }
             }
-
         }
-        logger.info("Finished creating a MATSim population.");
-        return matsimPopulation;
+        logger.info("Finished creating MATSim scenario.");
+        return scenario;
     }
 
     /**
@@ -147,52 +139,4 @@ public class SimpleCommuteModeChoiceMatsimScenarioAssembler implements MatsimSce
     private int defineWorkEndTime(Job job, int departureTime) {
         return departureTime + job.getWorkingTimeInSeconds().orElse(Math.max(0, (int) (8*3600 + SiloUtil.getRandomObject().nextGaussian() * 3600)));
     }
-
-    @Override
-    public Scenario assembleScenario(Config initialMatsimConfig, int year, TravelTimes travelTimes) {
-        Config config = createMatsimConfig(initialMatsimConfig);
-
-//        if(dataContainer.getTravelTimes() instanceof SkimTravelTimes) {
-//
-//        }
-//        if(year == properties.main.startYear) {
-//            logger.info("Referring to the simple MATSim scenario assembler for the base year as no travel times are available yet.");
-//            return new SimpleMatsimScenarioAssembler(dataContainer, properties).assembleScenario(initialMatsimConfig, year);
-//        } else {
-//            travelTimes = dataContainer.getTravelTimes();
-//        }
-
-
-        MutableScenario scenario = (MutableScenario) ScenarioUtils.loadScenario(config);
-        Population population = generateDemand(travelTimes);
-        scenario.setPopulation(population);
-
-        return scenario;
-    }
-
-    private Config createMatsimConfig(Config initialConfig) {
-        logger.info("Stating creating a MATSim config.");
-        Config config = ConfigUtils.loadConfig(initialConfig.getContext());
-        config.qsim().setFlowCapFactor(properties.transportModel.matsimScaleFactor);
-        config.qsim().setStorageCapFactor(properties.transportModel.matsimScaleFactor);
-
-        // TODO Add some switch here like "autoGenerateSimplePlans" or similar...
-        PlanCalcScoreConfigGroup.ActivityParams homeActivity = new PlanCalcScoreConfigGroup.ActivityParams("home");
-        homeActivity.setTypicalDuration(12*60*60);
-        config.planCalcScore().addActivityParams(homeActivity);
-
-        PlanCalcScoreConfigGroup.ActivityParams workActivity = new PlanCalcScoreConfigGroup.ActivityParams("work");
-        workActivity.setTypicalDuration(8*60*60);
-        config.planCalcScore().addActivityParams(workActivity);
-
-        config.vspExperimental().setVspDefaultsCheckingLevel(VspExperimentalConfigGroup.VspDefaultsCheckingLevel.warn);
-
-        logger.info("Finished creating a MATSim config.");
-        return config;
-    }
-
-
-
 }
-
-
