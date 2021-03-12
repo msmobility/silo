a39bc8cb45f43da3b7b9a0e6f8032e64fa574969
cllorca1 <carlos.llorca@tum.de>
2021-03-12 16:05:40 +0100

    move class between packages

--- useCases/bangkok/src/main/java/de/tum/bgu/msm/run/models/realEstate/BangkokPricingStrategy.java
+++ useCases/bangkok/src/main/java/de/tum/bgu/msm/run/models/realEstate/BangkokPricingStrategy.java
@@ -1,13 +1,14 @@
-package de.tum.bgu.msm.models.realEstate.pricing;
+package de.tum.bgu.msm.run.models.realEstate;
 
 import de.tum.bgu.msm.data.dwelling.Dwelling;
 import de.tum.bgu.msm.models.ScriptInputProvider;
+import de.tum.bgu.msm.models.realEstate.pricing.PricingStrategy;
 import de.tum.bgu.msm.util.js.JavaScriptCalculator;
 
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-public class BangkokPricingStrategy extends JavaScriptCalculator<Double> implements PricingStrategy{
+public class BangkokPricingStrategy extends JavaScriptCalculator<Double> implements PricingStrategy {
 
     private final static Reader reader = new InputStreamReader(ScriptInputProvider.getPricingScriptInput());
 
