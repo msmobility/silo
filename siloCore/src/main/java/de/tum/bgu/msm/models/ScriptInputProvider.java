c90396ddbb25c486bfff05414047aa6b21829bca
Nico <nico.kuehnel@tum.de>
2021-05-17 23:34:26 +0200

    replace renovation javascript calculator

--- siloCore/src/main/java/de/tum/bgu/msm/models/ScriptInputProvider.java
+++ siloCore/src/main/java/de/tum/bgu/msm/models/ScriptInputProvider.java
@@ -1,18 +1,10 @@
 package de.tum.bgu.msm.models;
 
-import java.io.InputStream;
-
 public final class ScriptInputProvider {
 
     private ScriptInputProvider() {
     }
 
-    private static InputStream renovationScriptInput
-            = ScriptInputProvider.class.getResourceAsStream("RenovationCalc");
 
-
-    public static InputStream getRenovationScriptInput() {
-        return renovationScriptInput;
-    }
 
 }
