a37a579ba7385005ffeeb307f1d56d111815f8d2
Nico <nico.kuehnel@tum.de>
2021-05-17 23:31:50 +0200

    replace moveOrNot Javascript calculator

--- siloCore/src/main/java/de/tum/bgu/msm/models/ScriptInputProvider.java
+++ siloCore/src/main/java/de/tum/bgu/msm/models/ScriptInputProvider.java
@@ -9,14 +9,10 @@ public final class ScriptInputProvider {
 
     private static InputStream renovationScriptInput
             = ScriptInputProvider.class.getResourceAsStream("RenovationCalc");
-    private static InputStream movesScriptInput  = ScriptInputProvider.class.getResourceAsStream("MovesOrNotCalc");
 
 
     public static InputStream getRenovationScriptInput() {
         return renovationScriptInput;
     }
 
-    public static InputStream getMovesScriptInput() {
-        return movesScriptInput;
-    }
 }
