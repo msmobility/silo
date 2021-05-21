09bbdc47025934e1896a8c6d222e8b0edf83db95
Qinnnnn <cindy.qinzhang@gmail.com>
2021-05-21 12:10:31 +0200

    write out original and matched sp files (pp,jj,hh.csv)

--- useCases/munich/src/main/java/de/tum/bgu/msm/scenarios/excessCommuteMatching/AdjustSyntheticPopulation.java
+++ useCases/munich/src/main/java/de/tum/bgu/msm/scenarios/excessCommuteMatching/AdjustSyntheticPopulation.java
@@ -1,11 +1,18 @@
 package de.tum.bgu.msm.scenarios.excessCommuteMatching;
 
 import de.tum.bgu.msm.DataBuilder;
+import de.tum.bgu.msm.data.dwelling.Dwelling;
+import de.tum.bgu.msm.data.dwelling.RealEstateDataManager;
+import de.tum.bgu.msm.data.household.Household;
+import de.tum.bgu.msm.data.household.HouseholdDataManager;
 import de.tum.bgu.msm.data.job.Job;
+import de.tum.bgu.msm.data.job.JobDataManager;
 import de.tum.bgu.msm.data.job.JobType;
 import de.tum.bgu.msm.data.person.Person;
+import de.tum.bgu.msm.io.HouseholdWriterMucDisability;
 import de.tum.bgu.msm.io.JobWriterMuc;
 import de.tum.bgu.msm.io.PersonWriterMuc;
+import de.tum.bgu.msm.io.output.HouseholdWriter;
 import de.tum.bgu.msm.properties.Properties;
 import de.tum.bgu.msm.schools.DataContainerWithSchools;
 import de.tum.bgu.msm.utils.SiloUtil;
@@ -13,18 +20,27 @@ import de.tum.bgu.msm.utils.SiloUtil;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class AdjustSyntheticPopulation {
+    private final static double SCALE_FACTOR = 0.05;
+    private final static long RANDOM_SEED = 1;
+    private final static String path = "F:\\models\\muc/siloMuc.properties";
+    private final static String matchBaseDirectory = "F:\\models\\msm-papers\\data\\thePerfectMatch";
+
 
     public static void main(String[] args) {
-        String path = "C:\\Users\\Nico\\tum\\fabilut\\gitproject\\muc/siloMuc.properties";
-        String matchBaseDirectory = "C:\\Users\\Nico\\tum\\msm-papers\\data\\thePerfectMatch";
 
         Properties properties = SiloUtil.siloInitialization(path);
         DataContainerWithSchools dataContainer = DataBuilder.getModelDataForMuc(properties, null);
         DataBuilder.read(properties, dataContainer);
+
+        scale(SCALE_FACTOR, dataContainer, RANDOM_SEED);
+        new PersonWriterMuc(dataContainer.getHouseholdDataManager()).writePersons(matchBaseDirectory +"/ppOrigin_"+ SCALE_FACTOR + "_" + RANDOM_SEED + ".csv");
+        new JobWriterMuc(dataContainer.getJobDataManager()).writeJobs(matchBaseDirectory + "/jjOrigin_" + SCALE_FACTOR + "_" + RANDOM_SEED + ".csv");
+        new HouseholdWriterMucDisability(dataContainer.getHouseholdDataManager(),dataContainer.getRealEstateDataManager()).writeHouseholds(matchBaseDirectory + "/hhOrigin_" + SCALE_FACTOR + "_" + RANDOM_SEED + ".csv");
+
 
         List<Match> totalMatches = new ArrayList<>();
         for(String sector: JobType.getJobTypes()) {
@@ -39,14 +55,15 @@ public class AdjustSyntheticPopulation {
             jobFromId.setWorkerID(personFromId.getId());
         }
 
-        new PersonWriterMuc(dataContainer.getHouseholdDataManager()).writePersons(matchBaseDirectory +"/ppMatched.csv");
-        new JobWriterMuc(dataContainer.getJobDataManager()).writeJobs(matchBaseDirectory + "/jjMatched.csv");
+        new PersonWriterMuc(dataContainer.getHouseholdDataManager()).writePersons(matchBaseDirectory +"/ppMatched_" + SCALE_FACTOR + "_" + RANDOM_SEED + ".csv");
+        new JobWriterMuc(dataContainer.getJobDataManager()).writeJobs(matchBaseDirectory + "/jjMatched_" + SCALE_FACTOR + "_" + RANDOM_SEED + ".csv");
+        new HouseholdWriterMucDisability(dataContainer.getHouseholdDataManager(),dataContainer.getRealEstateDataManager()).writeHouseholds(matchBaseDirectory + "/hhMatched_" + SCALE_FACTOR + "_" + RANDOM_SEED + ".csv");
 
     }
 
     private static List<Match> readMatches(String base, String sector) {
         List<Match> matches = new ArrayList<>();
-        File file = new File(base +"/finalMatches"+ sector+".csv");
+        File file = new File(base +"/finalMatches"+ sector+"_" + SCALE_FACTOR + "_" + RANDOM_SEED + "_oneStep.csv");
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             final String header = reader.readLine();
@@ -73,4 +90,44 @@ public class AdjustSyntheticPopulation {
             this.jobId = jobId;
         }
     }
+
+    public static void scale(double scaleFactor, DataContainerWithSchools dataContainer, long seed) {
+        Random random = new Random(seed);
+        //scale households
+        HouseholdDataManager householdDataManager = dataContainer.getHouseholdDataManager();
+        Collection<Household> households = householdDataManager.getHouseholds();
+
+        JobDataManager jobDataManager = dataContainer.getJobDataManager();
+        Collection<Job> jobs = jobDataManager.getJobs();
+        RealEstateDataManager realEstateDataManager = dataContainer.getRealEstateDataManager();
+
+        for (Household hh : households) {
+            if (random.nextDouble() > scaleFactor) {
+                int dwellingId = hh.getDwellingId();
+                for (Person person : hh.getPersons().values()) {
+                    person.setHousehold(null);
+                    householdDataManager.removePersonFromHousehold(person);
+                    householdDataManager.removePerson(person.getId());
+                    Job job = jobDataManager.getJobFromId(person.getJobId());
+                    if (job != null){
+                        jobDataManager.removeJob(job.getId());
+                    }
+                }
+                householdDataManager.removeHousehold(hh.getId());
+                realEstateDataManager.removeDwellingFromVacancyList(dwellingId);
+                realEstateDataManager.removeDwelling(dwellingId);
+
+            }
+        }
+
+        for (Job jj : jobs) {
+            if (jj.getWorkerId() == -1) {
+                if (random.nextDouble() > scaleFactor) {
+                    jobDataManager.removeJob(jj.getId());
+                }
+            }
+        }
+
+    }
+
 }
