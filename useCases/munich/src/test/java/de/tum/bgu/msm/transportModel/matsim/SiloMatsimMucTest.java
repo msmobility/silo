fe215b4f912f6c117c294fa7641c10c35cfdab53
nkuehnel <nkuehnel2@gmail.com>
2021-08-31 14:30:49 +0200

    delete code artifacts

--- useCases/munich/src/test/java/de/tum/bgu/msm/transportModel/matsim/SiloMatsimMucTest.java
+++ useCases/munich/src/test/java/de/tum/bgu/msm/transportModel/matsim/SiloMatsimMucTest.java
@@ -7,9 +7,6 @@ import de.tum.bgu.msm.container.ModelContainer;
 import de.tum.bgu.msm.io.output.DefaultResultsMonitor;
 import de.tum.bgu.msm.io.output.ResultsMonitor;
 import de.tum.bgu.msm.properties.Properties;
-import de.tum.bgu.msm.scenarios.noise.DataBuilderNoise;
-import de.tum.bgu.msm.scenarios.noise.ModelBuilderMucNoise;
-import de.tum.bgu.msm.scenarios.noise.NoiseDataContainerImpl;
 import de.tum.bgu.msm.schools.DataContainerWithSchools;
 import de.tum.bgu.msm.utils.SiloUtil;
 import junitx.framework.FileAssert;
@@ -66,10 +63,10 @@ public class SiloMatsimMucTest {
 
         Properties properties = SiloUtil.siloInitialization(path);
 
-        NoiseDataContainerImpl dataContainer = DataBuilderNoise.getModelDataForMuc(properties, config);
-        DataBuilderNoise.read(properties, dataContainer);
+        DataContainerWithSchools dataContainer = DataBuilder.getModelDataForMuc(properties, config);
+        DataBuilder.read(properties, dataContainer);
 
-        ModelContainer modelContainer = ModelBuilderMucNoise.getModelContainerForMuc(dataContainer, properties, config);
+        ModelContainer modelContainer = ModelBuilderMuc.getModelContainerForMuc(dataContainer, properties, config);
 
         ResultsMonitor resultsMonitor = new DefaultResultsMonitor(dataContainer, properties);
         SiloModel siloModel = new SiloModel(properties, dataContainer, modelContainer);
