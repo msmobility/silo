2882611de0b429d68017ae82360e74f8a1bf0b5f
cllorca1 <carlos.llorca@tum.de>
2022-07-05 09:54:52 +0200

    add portland use case

---
+++ useCases/portlandMe/src/main/java/de/tum/bgu/msm/run/RunSiloPortlandMe.java
@@ -0,0 +1,46 @@
+package de.tum.bgu.msm.run;
+
+import de.tum.bgu.msm.SiloModel;
+import de.tum.bgu.msm.container.DataContainer;
+import de.tum.bgu.msm.container.ModelContainer;
+import de.tum.bgu.msm.io.output.DefaultResultsMonitor;
+import de.tum.bgu.msm.io.output.MultiFileResultsMonitor;
+import de.tum.bgu.msm.io.output.ResultsMonitor;
+import de.tum.bgu.msm.properties.Properties;
+import de.tum.bgu.msm.utils.SiloUtil;
+import org.apache.log4j.Logger;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+
+public class RunSiloPortlandMe {
+
+    private final static Logger logger = Logger.getLogger(RunSiloPortlandMe.class);
+
+    public static void main(String[] args) {
+
+        Properties properties = SiloUtil.siloInitialization(args[0]);
+
+        Config config = null;
+        if (args.length > 1 && args[1] != null) {
+            config = ConfigUtils.loadConfig(args[1]);
+        }
+        logger.info("Started SILO land use model for the Maryland Statewide Model Area");
+        DataContainer dataContainer = DataBuilder.buildDataContainer(properties, config);
+        DataBuilder.readInput(properties, dataContainer);
+
+        ModelContainer modelContainer = ModelBuilderMstm.getModelContainerForMstm(dataContainer, properties, config);
+
+        ResultsMonitor resultsMonitor = new DefaultResultsMonitor(dataContainer, properties);
+        SiloModel model = new SiloModel(properties, dataContainer, modelContainer);
+        model.addResultMonitor(resultsMonitor);
+        ResultsMonitor mstmMonitor = new MstmMonitor(dataContainer, properties);
+        model.addResultMonitor(mstmMonitor);
+
+        MultiFileResultsMonitor monitor2 = new MultiFileResultsMonitor(dataContainer, properties);
+        model.addResultMonitor(monitor2);
+
+        model.runModel();
+        logger.info("Finished SILO.");
+    }
+
+}
