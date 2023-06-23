9e07d80c596c0e2f45110bc6032f6265a205d141
Kai Nagel <nagel@vsp.tu-berlin.de>
2023-06-23 12:30:42 +0200

    complete the regression test for fabiland.  Seems that the test (i.e. the tested code) is not deterministic

--- useCases/fabiland/src/test/java/run/RunFabilandTest.java
+++ useCases/fabiland/src/test/java/run/RunFabilandTest.java
@@ -26,26 +26,26 @@ public class RunFabilandTest{
 		try {
 			String [] args = {"./scenario/1r_ae.properties",
 					"./scenario/config_cap30_1-l_nes_smc.xml",
-//					"--config:controler.outputDirectory", utils.getOutputDirectory(),
-					"--config:controler.lastIteration", "1"
+//					"--config:controler.outputDirectory", utils.getOutputDirectory(), // has no effect; evidently overwritten by code
+					"--config:controler.lastIteration", "2"
 			} ;
 			RunFabiland.main( args ) ;
-//			{
-//				Population expected = PopulationUtils.createPopulation( ConfigUtils.createConfig() ) ;
-//				PopulationUtils.readPopulation( expected, utils.getInputDirectory() + "/output_plans.xml.gz" );
-//
-//				Population actual = PopulationUtils.createPopulation( ConfigUtils.createConfig() ) ;
-//				PopulationUtils.readPopulation( actual, utils.getOutputDirectory() + "/output_plans.xml.gz" );
-//
-//				boolean result = PopulationUtils.comparePopulations( expected, actual );
-//				Assert.assertTrue( result );
-//			}
-//			{
-//				String expected = utils.getInputDirectory() + "/output_events.xml.gz" ;
-//				String actual = utils.getOutputDirectory() + "/output_events.xml.gz" ;
-//				EventsFileComparator.Result result = EventsUtils.compareEventsFiles( expected, actual );
-//				Assert.assertEquals( EventsFileComparator.Result.FILES_ARE_EQUAL, result );
-//			}
+			{
+				Population expected = PopulationUtils.createPopulation( ConfigUtils.createConfig() ) ;
+				PopulationUtils.readPopulation( expected,  utils.getInputDirectory() + "10.output_plans.xml.gz" );
+
+				Population actual = PopulationUtils.createPopulation( ConfigUtils.createConfig() ) ;
+				PopulationUtils.readPopulation( actual, "scenario/scenOutput/base/matsim/10/10.output_plans.xml.gz" );
+
+				boolean result = PopulationUtils.comparePopulations( expected, actual );
+				Assert.assertTrue( result );
+			}
+			{
+				String expected = utils.getInputDirectory() + "/10.output_events.xml.gz" ;
+				String actual = "scenario/scenOutput/base/matsim/10/10.output_events.xml.gz" ;
+				EventsFileComparator.Result result = EventsUtils.compareEventsFiles( expected, actual );
+				Assert.assertEquals( EventsFileComparator.Result.FILES_ARE_EQUAL, result );
+			}
 
 		} catch ( Exception ee ) {
 			log.fatal("there was an exception: \n" + ee ) ;
