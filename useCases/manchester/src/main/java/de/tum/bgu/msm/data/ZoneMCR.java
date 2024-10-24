dadb9a580ded68b00aca94d150e76d4232137658
Qinnnnn <cindy.qinzhang@gmail.com>
2024-10-24 16:20:06 +0100

    implement Manchester-specified zone

---
+++ useCases/manchester/src/main/java/de/tum/bgu/msm/data/ZoneMCR.java
@@ -0,0 +1,25 @@
+package de.tum.bgu.msm.data;
+
+import de.tum.bgu.msm.data.geo.ZoneImpl;
+import org.locationtech.jts.geom.Coordinate;
+
+public class ZoneMCR extends ZoneImpl {
+
+    private final AreaTypes.RType areaType;
+    private final Coordinate popCentroidCoord;
+
+    public ZoneMCR(int id, float area, AreaTypes.RType areaType, Coordinate coordinate, Region region) {
+
+        super(id, area, region);
+        this.areaType = areaType;
+        this.popCentroidCoord = coordinate;
+    }
+
+    public AreaTypes.RType getAreaTypeRType() {
+        return areaType;
+    }
+
+    public Coordinate getPopCentroidCoord() {
+        return popCentroidCoord;
+    }
+}
