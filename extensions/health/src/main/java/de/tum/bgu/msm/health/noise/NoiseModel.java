f9e14e7e23e4d8997a39db651fab343086fa45fa
Qinnnnn <cindy.qinzhang@gmail.com>
2025-01-05 00:25:21 +0000

    Manchester Noise model

---
+++ extensions/health/src/main/java/de/tum/bgu/msm/health/noise/NoiseModel.java
@@ -0,0 +1,238 @@
+package de.tum.bgu.msm.health.noise;
+
+import de.tum.bgu.msm.container.DataContainer;
+import de.tum.bgu.msm.data.dwelling.RealEstateDataManager;
+import de.tum.bgu.msm.models.AbstractModel;
+import de.tum.bgu.msm.models.ModelUpdateListener;
+import de.tum.bgu.msm.properties.Properties;
+import de.tum.bgu.msm.utils.SiloUtil;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.noise.*;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.geometry.CoordUtils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+public class NoiseModel extends AbstractModel implements ModelUpdateListener {
+
+    private static final Logger logger = LogManager.getLogger(NoiseModel.class);
+
+    private final Config initialConfig;
+
+    private final RealEstateDataManager realEstateDataManager;
+    private int latestMatsimYear = -1;
+
+    private final NoiseReceiverPoints noiseReceiverPoints;
+
+
+    public NoiseModel(DataContainer data, Properties properties, Random random, Config initialConfig) {
+        super(data, properties, random);
+        this.initialConfig = initialConfig;
+        this.realEstateDataManager = data.getRealEstateDataManager();
+        this.noiseReceiverPoints = new NoiseReceiverPoints();
+    }
+
+    @Override
+    public void setup() {
+    }
+
+    @Override
+    public void prepareYear(int year) {
+
+    }
+
+    @Override
+    public void endYear(int year) {
+        updateImmissions(year);
+    }
+
+    @Override
+    public void endSimulation() {
+    }
+
+    private void updateImmissions(int year) {
+        logger.info("Updating noise immisisons for year " + year + ".");
+
+//        NoiseReceiverPoints newNoiseReceiverPoints = new NoiseReceiverPoints();
+//        NoiseReceiverPoints existingNoiseReceiverPoints = new NoiseReceiverPoints();
+//
+//        for (Dwelling dwelling : realEstateDataManager.getDwellings()) {
+//            Id<ReceiverPoint> id = Id.create(dwelling.getId(), ReceiverPoint.class);
+//            final NoiseReceiverPoint existing = noiseReceiverPoints.remove(id);
+//            if (existing == null) {
+//                newNoiseReceiverPoints.put(id, new NoiseReceiverPoint(id, CoordUtils.createCoord(dwelling.getCoordinate())));
+//            } else {
+//                existingNoiseReceiverPoints.put(id, existing);
+//            }
+//        }
+//
+//        this.noiseReceiverPoints.clear();
+//        this.noiseReceiverPoints.putAll(newNoiseReceiverPoints);
+//        this.noiseReceiverPoints.putAll(existingNoiseReceiverPoints);
+
+        //Temoparary
+
+        readBuidlingFile();
+
+        if(properties.main.startYear == year) {
+            latestMatsimYear = year;
+            calculateNoiseOffline(noiseReceiverPoints);
+        }
+
+        writeNoiseImission();
+        /*int counter65 = 0;
+        int counter55 = 0;
+        for (Dwelling dwelling : dataContainer.getRealEstateDataManager().getDwellings()) {
+            final Id<ReceiverPoint> id = Id.create(dwelling.getId(), ReceiverPoint.class);
+            if (noiseReceiverPoints.containsKey(id)) {
+                double lden = noiseReceiverPoints.get(id).getLden();
+                ((NoiseDwelling) dwelling).setNoiseImmision(lden);
+                if (lden > 55) {
+                    if (lden > 65) {
+                        counter65++;
+                    } else {
+                        counter55++;
+                    }
+                }
+            }
+        }
+        int total = dataContainer.getRealEstateDataManager().getDwellings().size();
+        int quiet = total - counter55 - counter65;
+        logger.info("Dwellings <55dB(A) : " + quiet + " (" + ((double) quiet) / total + "%)");
+        logger.info("Dwellings 55dB(A)-65dB(A) : " + counter55 + " (" + ((double) counter55) / total + "%)");
+        logger.info("Dwellings >65dB(A) : " + counter65 + " (" + ((double) counter65) / total + "%)");*/
+    }
+
+
+
+
+    private void calculateNoiseOffline(NoiseReceiverPoints noiseReceiverPoints) {
+        final String outputDirectoryRoot = properties.main.baseDirectory + "scenOutput/"
+                + properties.main.scenarioName + "/matsim/" + latestMatsimYear + "/thursday/car/";
+
+        Config config = ConfigUtils.loadConfig(initialConfig.getContext());
+        config.controller().setOutputDirectory(outputDirectoryRoot);
+        config.controller().setRunId(String.valueOf(latestMatsimYear));
+        Scenario scenario = ScenarioUtils.createMutableScenario(config);
+
+        scenario = ScenarioUtils.loadScenario(scenario.getConfig());
+
+        scenario.addScenarioElement(NoiseReceiverPoints.NOISE_RECEIVER_POINTS, noiseReceiverPoints);
+
+        NoiseConfigGroup noiseParameters = ConfigUtils.addOrGetModule(initialConfig, NoiseConfigGroup.class);
+        noiseParameters.setInternalizeNoiseDamages(false);
+        noiseParameters.setComputeCausingAgents(false);
+        noiseParameters.setComputeNoiseDamages(false);
+        noiseParameters.setComputePopulationUnits(false);
+        noiseParameters.setComputeAvgNoiseCostPerLinkAndTime(false);
+        noiseParameters.setThrowNoiseEventsCaused(false);
+        noiseParameters.setThrowNoiseEventsAffected(false);
+        noiseParameters.setNoiseComputationMethod(NoiseConfigGroup.NoiseComputationMethod.RLS19);
+        noiseParameters.setWriteOutputIteration(1);
+        double scalingFactor = properties.healthData.matsim_scale_factor_car;
+        noiseParameters.setScaleFactor(1./scalingFactor);
+        config.addModule(noiseParameters);
+
+        config.qsim().setEndTime(24 * 60 * 60);
+
+        noiseParameters.setConsiderNoiseBarriers(true);
+        noiseParameters.setConsiderNoiseReflection(true);
+        noiseParameters.setNoiseBarriersFilePath("/home/qin/models/manchester/input/buildingShapefile/mrcBuildings.geojson");
+        noiseParameters.setNoiseBarriersSourceCRS("EPSG:27700");
+        config.global().setCoordinateSystem("EPSG:27700");
+
+        NoiseOfflineCalculation noiseOfflineCalculation = new NoiseOfflineCalculation(scenario, outputDirectoryRoot);
+        noiseOfflineCalculation.run();
+
+    }
+
+    private void readBuidlingFile() {
+        String fileName = "/home/qin/models/manchester/input/buildingShapefile/dwelling_salford.csv";
+
+        String recString = "";
+        int recCount = 0;
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(fileName));
+            recString = in.readLine();
+
+            // read header
+            String[] header = recString.split(",");
+            int posId = SiloUtil.findPositionInArray("id", header);
+            int posCoordX = SiloUtil.findPositionInArray("coordX", header);
+            int posCoordY = SiloUtil.findPositionInArray("coordY", header);
+
+            // read line
+            while ((recString = in.readLine()) != null) {
+                    recCount++;
+                    String[] lineElements = recString.split(",");
+                    int index = Integer.parseInt(lineElements[posId]);
+                    double xCoordinate = Double.parseDouble(lineElements[posCoordX]);
+                    double yCoordinate = Double.parseDouble(lineElements[posCoordY]);
+
+                    Id<ReceiverPoint> id = Id.create(index, ReceiverPoint.class);
+                    noiseReceiverPoints.put(id, new NoiseReceiverPoint(id, CoordUtils.createCoord(xCoordinate, yCoordinate)));
+            }
+
+        } catch (IOException e) {
+            logger.fatal("IO Exception caught reading dwelling file: " + fileName, new RuntimeException());
+            logger.fatal("recCount = " + recCount + ", recString = <" + recString + ">", new RuntimeException());
+        }
+        logger.info("Finished reading " + recCount + " dwellings.");
+    }
+
+    private void writeNoiseImission() {
+        int counter65 = 0;
+        int counter55 = 0;
+
+        final String outputDirectory = properties.main.baseDirectory + "scenOutput/" + properties.main.scenarioName +"/";
+        String path = outputDirectory
+                + properties.realEstate.dwellingsFinalFileName
+                + "Noise_salford"
+                + "2021"
+                + ".csv";
+
+        logger.info("  Writing noise imission file to " + path);
+        PrintWriter pwd = SiloUtil.openFileForSequentialWriting(path, false);
+        pwd.print("id,noise");
+        pwd.println();
+
+
+
+        for (NoiseReceiverPoint point : noiseReceiverPoints.values()) {
+            double lden = point.getLden();
+            pwd.print(point.getId());
+            pwd.print(",");
+            pwd.print(lden);
+            pwd.println();
+
+            if (lden > 55) {
+                if (lden > 65) {
+                    counter65++;
+                } else {
+                    counter55++;
+                }
+            }
+
+        }
+
+        pwd.close();
+
+        int total = noiseReceiverPoints.size();
+        int quiet = total - counter55 - counter65;
+        logger.info("Dwellings <55dB(A) : " + quiet + " (" + ((double) quiet) / total + "%)");
+        logger.info("Dwellings 55dB(A)-65dB(A) : " + counter55 + " (" + ((double) counter55) / total + "%)");
+        logger.info("Dwellings >65dB(A) : " + counter65 + " (" + ((double) counter65) / total + "%)");
+
+
+    }
+}
---
+++ useCases/manchester/src/main/java/de/tum/bgu/msm/io/NoiseDwellingWriter.java
@@ -0,0 +1,62 @@
+package de.tum.bgu.msm.io;
+import de.tum.bgu.msm.data.dwelling.Dwelling;
+import de.tum.bgu.msm.data.dwelling.RealEstateDataManager;
+import de.tum.bgu.msm.io.output.DefaultDwellingWriter;
+import de.tum.bgu.msm.io.output.DwellingWriter;
+import de.tum.bgu.msm.health.noise.NoiseDwelling;
+import de.tum.bgu.msm.utils.SiloUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.PrintWriter;
+public class NoiseDwellingWriter implements DwellingWriter {
+
+    private final static Logger logger = LogManager.getLogger(DefaultDwellingWriter.class);
+    private final RealEstateDataManager realEstateDataManager;
+
+    public NoiseDwellingWriter(RealEstateDataManager realEstateDataManager) {
+        this.realEstateDataManager = realEstateDataManager;
+    }
+
+    @Override
+    public void writeDwellings(String path) {
+        logger.info("  Writing dwelling file to " + path);
+        PrintWriter pwd = SiloUtil.openFileForSequentialWriting(path, false);
+        pwd.print("id,zone,type,hhID,bedrooms,quality,monthlyCost,yearBuilt,coordX,coordY,noise");
+        pwd.println();
+
+        for (Dwelling dd : realEstateDataManager.getDwellings()) {
+            pwd.print(dd.getId());
+            pwd.print(",");
+            pwd.print(dd.getZoneId());
+            pwd.print(",\"");
+            pwd.print(dd.getType());
+            pwd.print("\",");
+            pwd.print(dd.getResidentId());
+            pwd.print(",");
+            pwd.print(dd.getBedrooms());
+            pwd.print(",");
+            pwd.print(dd.getQuality());
+            pwd.print(",");
+            pwd.print(dd.getPrice());
+            pwd.print(",");
+            pwd.print(dd.getYearBuilt());
+            pwd.print(",");
+            if(dd.getCoordinate() != null) {
+                pwd.print(dd.getCoordinate().x);
+                pwd.print(",");
+                pwd.print(dd.getCoordinate().y);
+            } else {
+                pwd.print("NULL,NULL");
+            }
+            pwd.print(",");
+            pwd.print(((NoiseDwelling)dd).getNoiseImmission());
+            pwd.println();
+            if (dd.getId() == SiloUtil.trackDd) {
+                SiloUtil.trackingFile("Writing dd " + dd.getId() + " to micro data file.");
+                SiloUtil.trackWriter.println(dd.toString());
+            }
+        }
+        pwd.close();
+    }
+}
