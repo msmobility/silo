2be38a18b17ac52c72bcf5200000eeb244f549c5
Qinnnnn <cindy.qinzhang@gmail.com>
2025-03-20 23:45:49 +0000

    tidy up codes in noise model

--- extensions/health/src/main/java/de/tum/bgu/msm/health/noise/NoiseModel.java
+++ extensions/health/src/main/java/de/tum/bgu/msm/health/noise/NoiseModel.java
@@ -3,6 +3,7 @@ package de.tum.bgu.msm.health.noise;
 import de.tum.bgu.msm.container.DataContainer;
 import de.tum.bgu.msm.data.Day;
 import de.tum.bgu.msm.data.dwelling.RealEstateDataManager;
+import de.tum.bgu.msm.health.data.DataContainerHealth;
 import de.tum.bgu.msm.models.AbstractModel;
 import de.tum.bgu.msm.models.ModelUpdateListener;
 import de.tum.bgu.msm.properties.Properties;
@@ -69,61 +70,21 @@ public class NoiseModel extends AbstractModel implements ModelUpdateListener {
     private void updateImmissions(int year) {
         logger.info("Updating noise immisisons for year " + year + ".");
 
-//        NoiseReceiverPoints newNoiseReceiverPoints = new NoiseReceiverPoints();
-//        NoiseReceiverPoints existingNoiseReceiverPoints = new NoiseReceiverPoints();
-//
-//        for (Dwelling dwelling : realEstateDataManager.getDwellings()) {
-//            Id<ReceiverPoint> id = Id.create(dwelling.getId(), ReceiverPoint.class);
-//            final NoiseReceiverPoint existing = noiseReceiverPoints.remove(id);
-//            if (existing == null) {
-//                newNoiseReceiverPoints.put(id, new NoiseReceiverPoint(id, CoordUtils.createCoord(dwelling.getCoordinate())));
-//            } else {
-//                existingNoiseReceiverPoints.put(id, existing);
-//            }
-//        }
-//
-//        this.noiseReceiverPoints.clear();
-//        this.noiseReceiverPoints.putAll(newNoiseReceiverPoints);
-//        this.noiseReceiverPoints.putAll(existingNoiseReceiverPoints);
-
-        //Temoparary
         if(properties.main.startYear == year) {
             latestMatsimYear = year;
             for(Day day : simulatedDays){
                 logger.info("Updating noise immisisons for year " + year + " day " + day + ".");
                 this.noiseReceiverPoints.clear();
-                readBuidlingFile();
+                //assemble Receiver Points
+                ((DataContainerHealth) dataContainer).getActivityLocations().values().stream().forEach(activityLocation -> {
+                    Id<ReceiverPoint> id = Id.create(activityLocation.getLocationId(), ReceiverPoint.class);
+                    noiseReceiverPoints.put(id, new NoiseReceiverPoint(id, CoordUtils.createCoord(activityLocation.getCoordinate().x, activityLocation.getCoordinate().y)));
+                });
+
                 calculateNoiseOffline(noiseReceiverPoints, day);
-                writeNoiseImission(day);
             }
         }
-
-
-        /*int counter65 = 0;
-        int counter55 = 0;
-        for (Dwelling dwelling : dataContainer.getRealEstateDataManager().getDwellings()) {
-            final Id<ReceiverPoint> id = Id.create(dwelling.getId(), ReceiverPoint.class);
-            if (noiseReceiverPoints.containsKey(id)) {
-                double lden = noiseReceiverPoints.get(id).getLden();
-                ((NoiseDwelling) dwelling).setNoiseImmision(lden);
-                if (lden > 55) {
-                    if (lden > 65) {
-                        counter65++;
-                    } else {
-                        counter55++;
-                    }
-                }
-            }
-        }
-        int total = dataContainer.getRealEstateDataManager().getDwellings().size();
-        int quiet = total - counter55 - counter65;
-        logger.info("Dwellings <55dB(A) : " + quiet + " (" + ((double) quiet) / total + "%)");
-        logger.info("Dwellings 55dB(A)-65dB(A) : " + counter55 + " (" + ((double) counter55) / total + "%)");
-        logger.info("Dwellings >65dB(A) : " + counter65 + " (" + ((double) counter65) / total + "%)");*/
     }
-
-
-
 
     private void calculateNoiseOffline(NoiseReceiverPoints noiseReceiverPoints, Day day) {
         final String outputDirectoryRoot = properties.main.baseDirectory + "scenOutput/"
@@ -187,9 +148,7 @@ public class NoiseModel extends AbstractModel implements ModelUpdateListener {
                     double xCoordinate = Double.parseDouble(lineElements[posCoordX]);
                     double yCoordinate = Double.parseDouble(lineElements[posCoordY]);
 
-                    Id<ReceiverPoint> id = Id.create(index, ReceiverPoint.class);
-                    noiseReceiverPoints.put(id, new NoiseReceiverPoint(id, CoordUtils.createCoord(xCoordinate, yCoordinate)));
-            }
+                    }
 
         } catch (IOException e) {
             logger.fatal("IO Exception caught reading dwelling file: " + fileName, new RuntimeException());
