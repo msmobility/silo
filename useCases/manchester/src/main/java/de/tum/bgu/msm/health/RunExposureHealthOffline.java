177f85d670cd9cc0026151f9269f5faab27719ee
Ismail Saadi <is558@medschl.cam.ac.uk>
2025-07-10 12:48:22 +0100

    test sunday with new approach

--- useCases/manchester/src/main/java/de/tum/bgu/msm/health/RunExposureHealthOffline.java
+++ useCases/manchester/src/main/java/de/tum/bgu/msm/health/RunExposureHealthOffline.java
@@ -57,15 +57,15 @@ public class RunExposureHealthOffline {
         SportPAModelMCR sportPAModelMCR = new SportPAModelMCR(dataContainer, properties, SiloUtil.provideNewRandom());
         AccidentModelMCR accidentModel = new AccidentModelMCR(dataContainer, properties, SiloUtil.provideNewRandom());
         HealthExposureModelMCR exposureModelMCR = new HealthExposureModelMCR(dataContainer, properties, SiloUtil.provideNewRandom(),config);
-        //DiseaseModelMCR diseaseModelMCR = new DiseaseModelMCR(dataContainer, properties, SiloUtil.provideNewRandom());
+        DiseaseModelMCR diseaseModelMCR = new DiseaseModelMCR(dataContainer, properties, SiloUtil.provideNewRandom());
 
         // runs
         sportPAModelMCR.endYear(2021);
         accidentModel.endYear(2021);
         exposureModelMCR.endYear(2021);
-        //diseaseModelMCR.setup();
-        //diseaseModelMCR.endYear(2021);
-        //dataContainer.endSimulation();
+        diseaseModelMCR.setup();
+        diseaseModelMCR.endYear(2021);
+        dataContainer.endSimulation();
 
         logger.info("Finished SILO.");
     }
